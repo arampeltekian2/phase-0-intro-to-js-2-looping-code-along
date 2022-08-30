dd7da2f310a5fbe73cfb932b448c1c907f8c5cf0
Aram <arampeltekian2@gmail.com>
2022-08-30 14:15:04 -0500

    commit tree c7c6bb3045549aa408af2441edc16afd9463f66f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Katelyn", "Armando", "Brock"]
+function writeCards(namesArray, event){
+let thankYouCards= [];   
+    console.log(namesArray, event);
+    for (let i=0; i < namesArray.length; i++){
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+        console.log(thankYouCards);
+    }
+    return thankYouCards;
+}
+
+function countDown(counter){ 
+//let countDown = 0;
+while (counter >= 0) {
+  console.log(counter--)
+}
+}
